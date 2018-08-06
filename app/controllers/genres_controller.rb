81aeda66be1fdbd68093bd8c7596452bbb61030d
Holden Altaffer <haltaffer@gmail.com>
2018-08-06 21:18:14 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,22 +1,14 @@
 class GenresController < ApplicationController
   def new
-    @genre = Genre.new
   end
 
   def create
-    @genre = Genre.new(song_params)
-    @genre.save
-    redirect_to genre_path(@genre)
   end
 
   def edit
-    @genre = Genre.find(params[:id])
   end
 
   def update
-    @genre = Genre.find(params[:id])
-    @genre.update(genre_params)
-    redirect_to genre_path(@genre)
   end
 
   def destroy
@@ -26,12 +18,5 @@ class GenresController < ApplicationController
   end
 
   def show
-    @genre = Genre.find(params[:id])
   end
-
-  private
-
-def genre_params
-  params.require(:genre).permit(:name)
-end
 end
